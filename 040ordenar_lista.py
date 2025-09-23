6212c49dbf7afe3b4c277ddc7f4bbba44f88303d
Raul-data <rauljusto7@outlook.com>
2025-09-23 19:50:21 +0200

    new

--- 040ordenar_lista.py
+++ 040ordenar_lista.py
@@ -3,10 +3,10 @@
 numeros = [5, 2, 9, 1, 5, 6]
 
 for i in range(len(numeros)):
-    for j in range(len(numeros)-i-1):
-        if numeros[j]>numeros[j+1]:
-            aux = numeros[j+1]
-            numeros[j+1] = numeros[j]
+    for j in range(len(numeros) - i - 1):
+        if numeros[j] > numeros[j + 1]:
+            aux = numeros[j + 1]
+            numeros[j + 1] = numeros[j]
             numeros[j] = aux
 
 print(numeros)
