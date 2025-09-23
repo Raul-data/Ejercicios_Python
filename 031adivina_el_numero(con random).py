5c6038afaf97f50da23f7e9b0c67528f8093751e
Raul-data <rauljusto7@outlook.com>
2025-09-23 19:51:21 +0200

    new

--- 031adivina_el_numero(con random).py
+++ 031adivina_el_numero(con random).py
@@ -4,11 +4,13 @@ num_usuario = int(input("Adivina el numero entre el 1 y el 10: "))
 contador = 0
 
 while contador <= 4:
-    num_aleatorio = random.randint(1,10)
+    num_aleatorio = random.randint(1, 10)
     contador += 1
 
     if num_usuario == num_aleatorio:
-        print(f"Felicidades, has adivinado el numero {num_aleatorio} en el intento {contador}")
+        print(
+            f"Felicidades, has adivinado el numero {num_aleatorio} en el intento {contador}"
+        )
         break
     elif contador == 4:
         print(f"Lo siento has agotado tus intentos, el numero era {num_aleatorio}")
@@ -16,4 +18,3 @@ while contador <= 4:
     else:
         print(f"Lo siento intentalo de nuevo")
         num_usuario = int(input("Adivina el numero entre el 1 y el 10: "))
-   
--- 032mi_primera_lista.py
+++ 032mi_primera_lista.py
@@ -3,8 +3,8 @@ mi_lista = []
 
 while True:
     dato = input("Introduce un dato (o escribe 'fin' para terminar): ")
-    if dato.lower() == 'fin':
+    if dato.lower() == "fin":
         break
     mi_lista.append(dato)
-    
+
 print(f"los datos que has dado son: {mi_lista}")
