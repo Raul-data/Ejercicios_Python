67d08a71f1e6970849d80c937a3e789d5f3e0d7e
Raul-data <rauljusto7@outlook.com>
2025-10-06 14:19:40 +0200

    new

--- 060genera_multiplos.py
+++ 060genera_multiplos.py
@@ -1,4 +1,10 @@
 # Pide al usuario un número y genera una lista con sus primeros n múltiplos, donde n también es dada por el usuario.
 
 num_usuario = int(input("Dime un numero para generar la lista de multiplos: "))
-#no le entiendo
+n = int(input("Dime de cuantos multiplos lo quieres: "))
+multiplos = []
+
+for i in range(n):
+    multiplos += [num_usuario * (i +1)]
+
+print(f"Los primeros {n} multiplos de {num_usuario} son: {multiplos}")
