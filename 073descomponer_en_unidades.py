abba573b7da8cf1fb79ef3dc79ca896718a61634
Raul-data <rauljusto7@outlook.com>
2025-10-14 17:07:12 +0200

    new

--- 073descomponer_en_unidades.py
+++ 073descomponer_en_unidades.py
@@ -1,1 +1,9 @@
-#Si el usuario introduce el número 369 devolverá 3 centenas, 6 decenas, 9 unidades
+# Si el usuario introduce el número 369 devolverá 3 centenas, 6 decenas, 9 unidades
+numero = int(input("Dime un numero para descomponer por unidades: "))
+
+centenas = numero // 100
+numero = numero % 100
+decenas = numero // 10
+unidades = numero % 10
+
+print(centenas, "centenas,", decenas, "decenas,", unidades, "unidades")
